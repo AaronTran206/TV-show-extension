b35022fa6f92adc582bb8974d8a3051e52e6bc26
Aaron Tran <aarontran.wa@gmail.com>
2022-03-05 03:28:22 -0700

    2nd commit

--- contentScript/contentScript.js
+++ contentScript/contentScript.js
@@ -3,6 +3,6 @@ console.log("Hello from the context script");
 const aTags = document.getElementsByTagName("a");
 for (const tag of aTags) {
   if (tag.textContent.includes("i")) {
-    tag.style = "background-color: yellow;";
+    // tag.style = "background-color: yellow;";
   }
 }
